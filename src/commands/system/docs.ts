3e0de2b1c6af9aeaca5a99c687df0986904c68cc
Conrad36 <47489117+Conrad36@users.noreply.github.com>
2021-09-21 10:53:08 -0400

    Created docs searching

---
+++ src/commands/system/docs.ts
@@ -0,0 +1,27 @@
+import axios from "axios"
+import { Command } from "types/command"
+
+
+const docs: Command = {
+   config: {
+        information: {
+            name: "docs", 
+            description: "search the discord.js docs for things", 
+            permLevel: "Bot Owner", 
+            category: "system"
+        }, 
+        aliases: ["search", "doc", "searchdocs"], 
+        neededValues: [
+            {
+                valueName: "query", 
+                joinTogether: true, 
+                index: 0
+            }
+        ], 
+       run: async(_client, commandData) => {
+          const query = commandData.values.get("query");
+          let emb = await axios("https://djsdocs.sorta.moe/v2/embed?src=master&q="+ query!.toString())
+          await commandData.reply({embeds: [emb.data]})
+       }
+}
+export default docs;
